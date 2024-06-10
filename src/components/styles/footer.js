1ab8152c64a90bc3c1de1214553ca5731e7205e8
Maysummer <udojyk@gmail.com>
2024-06-10 21:10:56 +0100

    implement footer

--- src/components/styles/footer.js
+++ src/components/styles/footer.js
@@ -6,5 +6,11 @@ export const Footer = styled.footer`
   justify-content: space-around;
   align-items: center;
   margin: 1em -20px 0 -20px;
-  border-top: 2em;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  border-top-right-radius: 1.5em;
+  border-top-left-radius: 1.5em;
+  padding: 1em 0;
+  box-shadow: 0 -10px 20px #cad0f0;
 `;
